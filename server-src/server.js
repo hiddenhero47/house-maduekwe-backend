5bd61585506d5d238f8b0f08d5eafcc8036128be
hidden hero <hiddenhero47pro@gmail.com>
2025-10-03 20:35:45 +0100

    chenges

--- server-src/server.js
+++ server-src/server.js
@@ -5,10 +5,9 @@ const colors = require("colors");
 const dotenv = require("dotenv").config();
 const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 6000;
 const path = require("path");
 const handleCors = require("./middleware/corsMiddleware");
-const { ensureAdminExists } = require("./helpers/ensureAdmin");
 
 connectDB();
 
