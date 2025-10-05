018cf7efbb1ffdcec925b464137fc5758457625d
hidden hero <hiddenhero47pro@gmail.com>
2025-10-05 21:49:39 +0100

    user log in update

--- server-src/controllers/userController.js
+++ server-src/controllers/userController.js
@@ -68,7 +68,7 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route POST /api/users/login
 // @access Public
 const loginUser = asyncHandler(async (req, res) => {
-  const { email, password } = req.body;
+  const { email, password, token } = req.body;
 
   // Check for user email
   const user = await User.findOne({ email });
