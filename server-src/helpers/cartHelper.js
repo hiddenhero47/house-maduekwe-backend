493ddceadfae930fc5a23d49b17727479c1db1c9
hidden hero <hiddenhero47pro@gmail.com>
2026-04-21 16:08:55 +0100

    changes

--- server-src/helpers/cartHelper.js
+++ server-src/helpers/cartHelper.js
@@ -60,6 +60,10 @@ const buildValidatedCartItems = (itemList, shopItemMap) => {
 
     const finalSelectedAttributes = [];
 
+    console.log(selectedMap, "selectedMap"); 
+    console.log(grouped, "grouped");
+    
+
     // -------------------------
     // SIZE (REQUIRED)
     // -------------------------
